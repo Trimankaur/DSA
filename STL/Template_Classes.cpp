bb03e0472817aaf7ab6a519a04150d8bd2f92279
Trimankaur <trimankaur1999@gmail.com>
2020-08-07 01:08:37 +0530

    Template Classes

---
+++ STL/Template_Classes.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#include<algorithm>
+
+using namespace std;
+
+//passing datatype as template so that we don't have to write the same code for different datatypes
+template<typename T>
+T Max(T x, T y)
+{
+	return (x>y)?x:y;
+}
+
+int main()
+{
+	cout<<Max<int>(1,3)<<endl;
+	cout<<Max<char>('a','c')<<endl;
+	return 0;
+}
